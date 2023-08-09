fd5b57861b20c69f394ce165833f7f03c59cd2ff
pablitopdg <pablitopdg@gmail.com>
2023-08-09 19:52:49 -0300

    Update prueba.java

---
+++ prueba.java
@@ -0,0 +1,11 @@
+
+/**
+ *
+ * @author Bangho
+ */
+public class prueba {
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println("mundo");
+    }
+}
